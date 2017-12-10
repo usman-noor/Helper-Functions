30deca1f867a7f8f2d20e0479666c631475ddf05
usman-noor <30604280+usman-noor@users.noreply.github.com>
2017-12-09 22:21:28 -0800

    Create Decorators_Example.py

---
+++ Decorators_Example.py
@@ -0,0 +1,70 @@
+from functools import wraps
+def raise_if_not_ints(*args):
+    for arg in args:
+        if type(arg) != int:
+            raise ValueError('Invalid param. Only Ints allowed')
+
+
+def build_improved_op(op):
+    @wraps(op)
+    def Ioperation(*args, **kwargs):
+        "Does nasty things with your CPU"
+        raise_if_not_ints(*args)
+        return op(*args, **kwargs)
+
+    # Ioperation.__name__ = op.__name__
+    # Ioperation.__doc__ = op.__doc__ 
+    return Ioperation
+
+
+# add = build_improved_op(add)
+@build_improved_op
+def add(*args):
+    "Receives numbers and returns the sum"
+    return sum(args)
+
+help(add)
+
+"""
+print(add(2, 3, 4, 8, 8))
+print(add(2, 3))
+
+@build_improved_op
+def square(x):
+    return x ** 2
+
+print(square('a'))
+
+"""
+
+
+
+
+
+
+
+
+
+
+"""
+@build_improved_op
+def multiply(x, y):
+    return x * y
+
+# multiply = build_improved_op(multiply)
+
+@build_improved_op
+def subtract(x, y):
+    return x - y
+
+# subtract = build_improved_op(subtract)
+
+
+res1 = add('ab','cd')
+print(res1)
+
+res2 = multiply(2, 'a')
+print(res2)
+
+
+"""
