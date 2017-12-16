a01617fd9e698b61b59de8760c209bba40c70424
usman-noor <30604280+usman-noor@users.noreply.github.com>
2017-12-15 19:50:39 -0800

    Create Decorators-Class Based.py

---
+++ Decorators-Class Based.py
@@ -0,0 +1,20 @@
+class decorator_class(object):
+  
+  def __init__(self, original_function):
+    self.original_function = original_function
+    
+  def __call__(self,*args, **kwargs):
+    print('call method executed this before {}'.format(self.original_function.__name__))
+    return self.original_function(*args, **kwargs)
+    
+@decorator_class    
+def display():
+  print('display function ran')
+
+@decorator_class
+def display_info(name, age):
+  print('display function ran with arguments ({}, {})'.format(name, age))
+  
+display() 
+  
+display_info('John', 25)
